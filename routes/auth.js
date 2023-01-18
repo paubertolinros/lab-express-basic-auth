bb3cbb20c09b443c4f376250dc63793d8ade7c8a
Pau Bertolin <paulaberos@gmail.com>
2023-01-18 14:22:43 +0100

    iteration 1 and 2, half. Done

---
+++ routes/auth.js
@@ -0,0 +1,71 @@
+const router = require("express").Router();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const User = require('../models/User.model');
+
+/* GET sign up form view */
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+/* POST sign up form get data to DB */
+router.post("/signup", async (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.render('auth/signup', { error: 'All fields are necessary!' });
+    return;
+  }
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,}/;
+  if (!regex.test(password)) {
+    res.render('auth/signup', {error: 'Password needs to contain at lesat 7 characters, one number, one lowercase an one uppercase letter.'})
+    return;
+  }
+  try {
+    const ifUserInDB = await User.findOne({ username: username });
+    if (ifUserInDB) {
+      res.render('auth/signup', { error: `There alredy is a user with username ${username}` });
+      return
+    } else {
+      const salt = await bcrypt.genSalt(saltRounds);
+      const passwordHash = await bcrypt.hash(password, salt);
+      const user = await User.create({ username, passwordHash });
+      res.render('auth/profile', user);
+    }
+  } catch (error) {
+    next(error)
+  }
+});
+
+/* GET log in form view */
+router.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+
+/* POST log in form get data to DB */
+router.post('/login', async (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username | !password) {
+    res.render('auth/login', { error: 'username or password do not match' });
+    return;
+  }
+  try {
+    const ifUserInDB = await User.findOne({ username: username });
+    if (!ifUserInDB) {
+      res.render('auth/login', { error: `${username} doesn't exist here!` })
+      return
+    } else {
+      const passwordMatch = await bcrypt.compare(password, ifUserInDB.passwordHash);
+      if (passwordMatch) {
+        res.render('auth/profile', ifUserInDB);
+      } else {
+        res.render('auth/login', { error: 'Unable to authenticate user :(' });
+        return
+      }
+    }
+  } catch (error) {
+    next(error)
+  }
+});
+
+
+module.exports = router;
